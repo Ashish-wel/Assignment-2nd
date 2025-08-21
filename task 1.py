c0c433565655002baa98ff0d6b7e16da05c5a815
Ashish-wel <ashishnagda34@gmail.com>
2025-08-21 23:23:19 +0530

    Create task 1.py

---
+++ task 1.py
@@ -0,0 +1,19 @@
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+  print(f" number {num} is an even.")
+else:
+  print(f" number {num} is an odd.")
+
+
+-- Output --
+Enter a number: 8
+number 8 is an even.
+
+Enter a number: 9
+number 9 is an odd.
+
+
+  
+
+    
